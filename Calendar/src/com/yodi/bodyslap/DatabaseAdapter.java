c0a1e438e65d367b1342c6eb8a7489e2149b52ae
Yodiaditya <bashlook@gmail.com>
2012-12-22 16:31:40 +0800

    refactoring packages name to avoid conflict common activity name

---
+++ Calendar/src/com/yodi/bodyslap/DatabaseAdapter.java
@@ -0,0 +1,10 @@
+package com.yodi.bodyslap;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseAdapter {
+	public static final String DATABASE_NAME = "bodyslap";
+	public static final String DATABASE_TABLE ="tracker";
+	public static final Float DATABASE_VERSION = 0.1f;
+	
+}
